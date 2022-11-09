b2cb97291a72c8fccafbef71d3dd335d8806785d
Anmar Hani <76432762+AnmarHani@users.noreply.github.com>
2022-11-09 13:39:16 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,13 +1,27 @@
 #$simulator = Read-Host "Please enter your age"
 $path = Get-Location
 
-Write-Host "Please Download & Setup Python From This URL:"
-Write-Host "https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe"
-Read-Host "Please enter continue"
+if (!(Test-Path "$path\Installers")){
+  New-Item -Path $path -Name "Installers" -ItemType "directory"
+}
 
-Write-Host "Please Download & Setup GIT From This URL:"
-Write-Host "https://github.com/git-for-windows/git/releases/download/v2.38.1.windows.1/Git-2.38.1-64-bit.exe"
-Read-Host "Please enter continue"
+cd Installers
+if (!(Test-Path "$path\Installers\python-3.11.0-amd64.exe")){
+  Invoke-WebRequest "https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe" -OutFile "$path\Installers\python-3.11.0-amd64.exe"
+  .\"python-3.11.0-amd64.exe"
+  Read-Host "Continue If Finished"
+}
+if (!(Test-Path "$path\Installers\Git-2.38.1-64-bit.exe")){
+  Invoke-WebRequest "https://github.com/git-for-windows/git/releases/download/v2.38.1.windows.1/Git-2.38.1-64-bit.exe" -OutFile "$path\Installers\Git-2.38.1-64-bit.exe"
+  .\"Git-2.38.1-64-bit.exe"
+  Read-Host "Continue If Finished"
+}
 
-Write-Host "Please Restart Your Computer"
-Restart-Computer
+[System.Windows.MessageBox]::Show("Please Restart Your Computer!", "IMPORTANT MESSAGE", "OK", "Warning")
+
+
+
+# Invoke-WebRequest $myDownloadUrl -OutFile c:\file.ext
+# if (!(Test-Path -path $exactadminfile)){
+  
+# }
