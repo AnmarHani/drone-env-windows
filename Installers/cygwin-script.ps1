17f90b7f1d4eab691abf3ba14ccfb5704f6eca85
Anmar Hani <76432762+AnmarHani@users.noreply.github.com>
2022-11-17 21:49:58 +0300

    Update cygwin-script.ps1

--- Installers/cygwin-script.ps1
+++ Installers/cygwin-script.ps1
@@ -1,5 +1,14 @@
 $command0 = "cd $($args[0]):\"
 $command1 = "cygwin64\bin\bash.exe"
 
+Write-Host "----------------------------------------------"
+Write-Host "Please Run This:"
+Write-Host " "
+Write-Host "cd $($args[1])"
+Write-Host "----------------------------------------------"
+Write-Host "Then Run This:"
+Write-Host " "
+Write-Host "$($args[2])"
+
 Invoke-Expression $command0
 Invoke-Expression $command1
